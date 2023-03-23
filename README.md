ae4c3a9149aff702c47a6ba6c13977b49f6bfab7
LN <Lukenickerson@users.noreply.github.com>
2023-03-23 18:18:02 -0400

    initial methods from old prototype

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rocket-boots-three-toolbox
-Utilities for three js
+***Utilities for three js***
+
+To Install:
+
+```
+npm install github:rocket-boots/rocket-boots-three-toolbox
+```
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import clamp from './clamp.js';
+import MouseWheelWatcher from './MouseWheelWatcher.js';
+import Renderer from './Renderer.js';
+import ThirdPersonCamera from './ThirdPersonCamera.js';
+
+export {
+	clamp,
+	MouseWheelWatcher,
+	Renderer,
+	ThirdPersonCamera,
+};
